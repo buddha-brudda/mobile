71d1cddae813993b26f3654b44517eecf0974993
cl <cl@startup.org.sg>
2014-09-13 19:22:29 -0700

    added 'time-ago'

--- www/js/services.js
+++ www/js/services.js
@@ -2,9 +2,21 @@ angular.module('starter.services', [])
 
 .controller('AlertCenterCtrl', ['$http', function($http){
   var alertCenter = this;
+  alertCenter.notificationList = [];
+  // alertCenter.notificationList = [{date:"2014-09-14T00:50:30.048Z"},{date:"2014-09-14T00:40:30.048Z"}];
+  notificationList_dateISOString_toDateAgo(alertCenter.notificationList);
   $http.get("http://buddhabrudda.mybluemix.net/notifications/4699553379")
   .success(function(data){
     alertCenter.notificationList = data;
+    // alertCenter.notificationList = [{time:"2014-09-14T00:50:30.048Z"},{time:"2014-09-14T00:40:30.048Z"}];
+    notificationList_dateISOString_toDateAgo(alertCenter.notificationList);
   });
 }]);
 
+function notificationList_dateISOString_toDateAgo(notificationList){
+  for (var i=0;i<notificationList.length; ++i){
+    var dateISOString = notificationList[i].date;
+    notificationList[i].ago = moment(dateISOString).fromNow();
+  }
+  return notificationList;  
+}
